be44b5eacc5fcbded8c574651e39462373efeca2
DominikZawadzkii <domekkozak@o2.pl>
2020-05-20 10:56:15 +0200

    first commit

---
+++ aaaa.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+using namespace std; 
+	int t[1000] ; 
+	for ( int i = 0; i <1000 i++) {
+		if (zmienna == t[i] )
+		return false;
+		else return true; 
+	}
+ int main ()
+{
+	ifstream wej;
+	ofstream wyj; 
+	
+		wej.open("wej.txt");
+		wyj.open("wyj.txt");
+		int licznik = 0;
+		
+			if (wejscie.good()){
+			while(!wej.eof())
+			{
+				for (int: i = 0; i < 1000; i++)
+				wej >> t [i] ; 
+			}
+		for(int i = 0; i>= licznik; i++) {
+			if(i == licznik){
+				if(a_liczba(licznik) == true)
+				t[licznik] = i;
+					wyjscie << " [ \n { \n "liczba" :" " << i;
+			}
+			licznik++;
+	}
+	wej.close();
+	wyj.close();
+return 0; 
+}
